7a20f83bc09332008973408613b1298d445b76d2
meuzi8122 <kagiakete1234@gmail.com>
2023-04-24 11:56:43 +0900

    引数不足の条件分岐追加

--- bin/bookmark_cli.dart
+++ bin/bookmark_cli.dart
@@ -5,15 +5,23 @@ import 'package:bookmark_cli/bookmark_cli.dart';
 void main(List<String> arguments) async {
   if (arguments.length == 0) {
     print(
-        'コマンド一覧\n list   [キーワード]   説明文にキーワードを含むブックマーク一覧を表示(キーワードは省略可 その場合全件表示)\n upsert [url] [説明文] ブックマークを登録(登録済みのURLなら更新)\n delete [url]          ブックマークを削除');
+        'コマンド一覧\n list   [キーワード]   説明文にキーワードを含むブックマーク一覧を表示(キーワードは省略可 その場合全件表示)\n upsert [説明文] [url] ブックマークを登録(登録済みのURLなら更新)\n delete [url]          ブックマークを削除');
     exit(0);
   }
 
   if (arguments[0] == 'list') {
     await BookmarkCmd().findBookmarks(arguments.length > 1 ? arguments[1] : '');
   } else if (arguments[0] == 'upsert') {
+    if (arguments.length < 3) {
+      print('[ERROR] urlと説明文が不足しています');
+      exit(1);
+    }
     await BookmarkCmd().upsertBookmark(arguments[1], arguments[2]);
   } else if (arguments[0] == 'delete') {
+    if (arguments.length < 2) {
+      print('[ERROR] urlが不足しています');
+      exit(1);
+    }
     await BookmarkCmd().deleteBookmark(arguments[1]);
   } else {
     print('[ERROR] コマンドが見つかりません');
