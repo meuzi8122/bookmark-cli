e9955b24bfac9a4d8fa9c2a4f4594ed34753a5e5
meuzi8122 <kagiakete1234@gmail.com>
2023-04-24 11:16:36 +0900

    正常終了/異常終了を追加

--- bin/bookmark_cli.dart
+++ bin/bookmark_cli.dart
@@ -1,18 +1,23 @@
+import 'dart:io';
+
 import 'package:bookmark_cli/bookmark_cli.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   if (arguments.length == 0) {
     print('[ERROR] コマンドが指定されていません');
-    return;
+    exit(1);
   }
 
   if (arguments[0] == 'list') {
-    BookmarkCmd().findBookmarks(arguments.length > 1 ? arguments[1] : '');
+    await BookmarkCmd().findBookmarks(arguments.length > 1 ? arguments[1] : '');
   } else if (arguments[0] == 'upsert') {
-    BookmarkCmd().upsertBookmark(arguments[1], arguments[2]);
+    await BookmarkCmd().upsertBookmark(arguments[1], arguments[2]);
   } else if (arguments[0] == 'delete') {
-    BookmarkCmd().deleteBookmark(arguments[1]);
+    await BookmarkCmd().deleteBookmark(arguments[1]);
   } else {
     print('[ERROR] コマンドが見つかりません');
+    exit(1);
   }
+
+  exit(0);
 }
